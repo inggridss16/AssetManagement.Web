0fa909b4b8c5d76d84cc19f34df5f5fa0178ff4d
inggridss16 <inggridss16@gmail.com>
2025-11-07 22:12:49 +0700

    maintenance tab rules

--- wwwroot/js/asset-form.js
+++ wwwroot/js/asset-form.js
@@ -25,11 +25,44 @@ function updateSubcategoryDropdown(selectedCategory, $subcategoryDropdown) {
     }
 }
 
-// This block runs when the DOM is ready. It attaches the event handler.
+/**
+ * Hides the Maintenance tab if the subcategory is a type of furniture.
+ */
+function toggleMaintenanceTab() {
+    var furnitureSubcategories = ["Chair", "Table", "Cabinet"];
+    var currentSubcategory = $("#subcategoryDropdown").val();
+    var $maintenanceTab = $('#maintenance-tab');
+
+    // Only proceed if the tab exists on the page
+    if ($maintenanceTab.length === 0) {
+        return;
+    }
+
+    if (furnitureSubcategories.indexOf(currentSubcategory) > -1) {
+        $maintenanceTab.hide();
+
+        // If the maintenance tab was active, switch to the details tab
+        if ($maintenanceTab.hasClass('active')) {
+            var tab = new bootstrap.Tab(document.querySelector('#asset-details-tab'));
+            tab.show();
+        }
+    } else {
+        $maintenanceTab.show();
+    }
+}
+
+// This block runs when the DOM is ready.
 $(document).ready(function () {
-    $("#categoryDropdown").change(function () {
-        var selectedCategory = $(this).val();
-        var $subcategoryDropdown = $("#subcategoryDropdown");
-        updateSubcategoryDropdown(selectedCategory, $subcategoryDropdown);
+    var $categoryDropdown = $("#categoryDropdown");
+    var $subcategoryDropdown = $("#subcategoryDropdown");
+
+    // This event handler is for ALL forms using this script.
+    $categoryDropdown.on('change', function () {
+        updateSubcategoryDropdown($(this).val(), $subcategoryDropdown);
+        // Trigger the change event on the subcategory to ensure chained logic runs.
+        $subcategoryDropdown.trigger('change');
     });
+
+    // This event handler will only affect pages that have the #maintenance-tab.
+    $subcategoryDropdown.on('change', toggleMaintenanceTab);
 });
